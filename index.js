738b25c76cdb82f2efdb70f5f55c8978d93b773c
Darshil Aggarwal <darshil@Darshils-MacBook-Air.local>
2025-01-25 16:40:53 +0530

    added edit file functionality

--- index.js
+++ index.js
@@ -16,11 +16,47 @@ app.get('/', (req, res) => {
   })
 })
 
+app.get('/file/:filename', (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    if (err) {
+      console.error(err);
+      return res.status(404).send('File not found');
+    }
+    res.render("display", {filename: req.params.filename, filedata: filedata});
+    console.log(filedata);
+    console.log(req.params.filename);
+  });
+})
+
+app.get('/edit/:filename', (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render('edit', {
+      filename: req.params.filename,
+      filedata: filedata
+    });
+  });
+})
+
+app.post('/edit', (req, res) => {
+  fs.writeFile(`./files/${req.body.filename}`, req.body.filedata, (err) => {
+    res.redirect('/');
+  });
+})
+
+app.post('/edit2', (req, res) => {
+  fs.writeFile(`./files/${req.body.previous2}`, `./files/${req.body.new2}`, (err) => {
+    res.redirect('/');
+  });
+})
+
+
 app.post('/create', (req, res) => {
   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt` , req.body.details , (err)=>{
     res.redirect('./')
   })  
 })
+
+ 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
